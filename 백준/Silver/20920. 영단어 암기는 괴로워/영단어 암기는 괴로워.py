ae3973a8d8a8d7e0786b5abcf693300c8982bcf9
Sujin An <139312979+sujinann@users.noreply.github.com>
2023-12-02 16:22:50 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 480 ms, Memory: 139032 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -11,7 +11,6 @@ for i in range(n):
             d[w] += 1
 
 d = sorted(d.items(), key= lambda x :(-x[1], -len(x[0]), x[0]))
-for i in range(len(d)):
-    d[i] = list(d[i])
+
 for i in range(len(d)):   
     print(d[i][0])
