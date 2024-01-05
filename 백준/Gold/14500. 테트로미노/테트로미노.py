0124ee27ec43c203df80da6387ab50b9d8afcacd
Sujin An <139312979+sujinann@users.noreply.github.com>
2024-01-05 13:57:14 +0900

    [Gold IV] Title: 테트로미노, Time: 5940 ms, Memory: 306356 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -8,26 +8,33 @@ dy = [1, -1, 0, 0]
 
 answer = []
 
-def dfs(x, y, visited, path, cnt, t):
-    if cnt == 3:
-        answer.append(path)
+def dfs(node, graph, visited, s, t):
+    if len(visited) == 4:
+        answer.append(s)
         return
     
+    x = node[0]
+    y = node[1]
+
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
 
-        if 0 <= nx < n and 0 <= ny < m:
-            if [nx, ny] not in visited:
-                visited.append([nx, ny])
-                dfs(nx, ny, visited, path + graph[nx][ny], cnt+1, t)
-                visited.pop()
-            else:
-                if cnt == 2 and t == 0:
-                    dfs(nx, ny, visited, path, cnt, t+1)
+        if nx < 0 or nx >= n or ny < 0 or ny >= m:
+            continue
 
+        nnode = [nx, ny]
+
+        if nnode not in visited:
+            visited.append(nnode)
+            dfs(nnode, graph, visited, s+graph[nx][ny], t)
+            visited.pop()
+        else:
+            if len(visited) == 3 and t == 0:
+                dfs(nnode, graph, visited, s, t+1)
+            
 for i in range(n):
     for j in range(m):
-        dfs(i, j, [[i, j]], graph[i][j], 0, 0)
+        dfs([i, j], graph, [[i,j]], graph[i][j], 0)
 
 print(max(answer))
