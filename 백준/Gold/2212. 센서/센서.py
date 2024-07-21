1c32d80f3a0783f1a0c6088fcdcf8800d7cb4ed9
Sujin An <139312979+sujinann@users.noreply.github.com>
2024-07-21 11:11:41 +0900

    [Gold V] Title: 센서, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.py
+++ 백준/Gold/2212. 센서/센서.py
@@ -3,9 +3,7 @@ k = int(input())
 l = list(map(int, input().split()))
 l.sort()
 
-dist = []
-for i in range(n-1):
-    dist.append(l[i+1] - l[i])
+dist = [l[i+1] - l[i] for i in range(n-1)]
 
 dist.sort()
 print(sum(dist[:n-k]))
