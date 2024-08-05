2711584e89033dbb00a78e507d6926ce2674b2af
Sujin An <139312979+sujinann@users.noreply.github.com>
2024-08-05 13:49:51 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 672 ms, Memory: 220444 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -0,0 +1,52 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+
+n, m = map(int, input().strip().split())
+
+maps = []
+for i in range(n):
+    maps.append(list(map(int, input().strip())))
+
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
+
+visited = [[False]*m for i in range(n)]
+
+def bfs(x, y, cnt, p):
+    q = deque()
+    q.append([x, y, cnt, p])
+    maps[x][y] = cnt
+
+    while q:
+        x, y, cnt, p = q.popleft()
+        if x == n-1 and y == m-1:
+            if cnt > 0:
+                return cnt
+            else:
+                return -cnt
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0<=nx<n and 0<=ny<m:
+                if p == 0:
+                    if maps[nx][ny] <= 0 and not visited[nx][ny]:
+                        maps[nx][ny] = cnt + 1
+                        q.append([nx, ny, cnt+1, p])
+
+                    if maps[nx][ny] == 1:
+                        maps[nx][ny] = -cnt - 1
+                        visited[nx][ny] = True
+                        q.append([nx, ny, -cnt-1, p+1])
+                
+                else:
+                    if maps[nx][ny] == 0:
+                        maps[nx][ny] = cnt-1
+                        q.append([nx, ny, cnt-1, p])
+
+    return -1
+
+print(bfs(0, 0, 1, 0))
