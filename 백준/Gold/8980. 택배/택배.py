f2fede11602bb33c8b20c4199b6403b363436f42
Sujin An <139312979+sujinann@users.noreply.github.com>
2024-09-01 16:53:45 +0900

    [Gold I] Title: 택배, Time: 92 ms, Memory: 108080 KB, Score: 32 point -BaekjoonHub

--- 백준/Gold/8980. 택배/택배.py
+++ 백준/Gold/8980. 택배/택배.py
@@ -21,27 +21,16 @@ for i in range(m):
             total -= p[1]
             answer += p[1]
 
-    if l[i][2] + total > c:
-        temp = l[i][2]
-        while hq and hq[-1][0] > l[i][1]:
-            if temp <= 0:
-                break
+    heapq.heappush(hq, [l[i][1], l[i][2]])
+    total += l[i][2]
+
+    if total > c:            
+        while hq and hq[-1][1] < total - c:
+            total -= hq.pop()[1]
             
-            if temp >= hq[-1][1]:
-                t = hq.pop()[1]
-                temp -= t
-                heapq.heappush(hq, [l[i][1], t])
-            else:
-                hq[-1][1] -= temp
-                heapq.heappush(hq, [l[i][1], temp])
-                temp = 0
-        else:
-            if total < c:
-                heapq.heappush(hq, [l[i][1], c-total])
-                total = c
-    else:
-        heapq.heappush(hq, [l[i][1], l[i][2]])
-        total += l[i][2]
+        if hq:
+            hq[-1][1] -= total - c
+            total = c
 
 while hq:
     answer += hq.pop()[1]
